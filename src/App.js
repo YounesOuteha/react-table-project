e50c3e710696debf75af3a684322d4388cfdd37c
YounesOuteha <younes.outeha@gmail.com>
2021-01-17 11:03:28 +0100

    pagination Next Previous with N° of pages

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import SortingTable from './components/SortingTable';
 import FilteringTable from './components/FilteringTable';
+import PaginationTable from './components/PaginationTable';
 
 function App() {
   return (
     <div className="App">
-      <FilteringTable/>
+      <PaginationTable/>
     </div>
   );
 }
